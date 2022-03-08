86d8f01c15b061e9307761da56059e8e9ffe6fa9
derekyoung13 <99927525+derekyoung13@users.noreply.github.com>
2022-03-08 13:26:22 -0800

    Update create_sch_task.ps1

--- create_sch_task.ps1
+++ create_sch_task.ps1
@@ -3,6 +3,6 @@ $trigger = New-ScheduledTaskTrigger -Once -At "3/9/2022 10:00:00 AM"
 $principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object -expand UserName)
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
 Register-ScheduledTask Notepad -InputObject $task
-Start-ScheduledTask -TaskName O365_Migration
+Start-ScheduledTask -TaskName GRA_O365_Migration
 Start-Sleep -Seconds 5
-Unregister-ScheduledTask -TaskName notepad -Confirm:$false
+Unregister-ScheduledTask -TaskName GRA_O365_Migration -Confirm:$false
