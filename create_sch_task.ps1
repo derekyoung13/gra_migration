adeb4868e4c1b7071504f34eb5c6fbbdb74cdb13
derekyoung13 <99927525+derekyoung13@users.noreply.github.com>
2022-03-15 13:06:17 -0700

    Delete create_sch_task.ps1

--- create_sch_task.ps1
+++
@@ -1,5 +0,0 @@
-$action = New-ScheduledTaskAction -Execute “powershell.exe” -Argument "-executionpolicy bypass -file c:\programdata\GRA_O365\migration.ps1"
-$trigger = New-ScheduledTaskTrigger -Once -At "3/9/2022 10:00:00 AM"
-$principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object -expand UserName)
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
-Register-ScheduledTask GRA_O365_Migration -InputObject $task
